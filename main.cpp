5b7c70b2be896108ad509c79ffdc41d14efbbe2d
czoido <mrgalleta@gmail.com>
2021-01-25 15:28:14 +0100

    draw triangle

--- main.cpp
+++ main.cpp
@@ -73,11 +73,49 @@ GLFWwindow *createWindow()
 	return window;
 }
 
+void create_triangle(unsigned int &vbo, unsigned int &vao, unsigned int &ebo)
+{
+
+	// create the triangle
+	float triangle_vertices[] = {
+		0.0f, 0.25f, 0.0f,	  // position vertex 1
+		1.0f, 0.0f, 0.0f,	  // color vertex 1
+		0.25f, -0.25f, 0.0f,  // position vertex 1
+		0.0f, 1.0f, 0.0f,	  // color vertex 1
+		-0.25f, -0.25f, 0.0f, // position vertex 1
+		0.0f, 0.0f, 1.0f,	  // color vertex 1
+	};
+	unsigned int triangle_indices[] = {
+		0, 1, 2};
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(triangle_vertices), triangle_vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangle_indices), triangle_indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
 int main(int, char **)
 {
 	GLFWwindow *window = createWindow();
 	if (window == NULL)
 		return 1;
+
+	// create our geometries
+	unsigned int vbo, vao, ebo;
+	create_triangle(vbo, vao, ebo);
+
+	// init shader
+	Shader triangle_shader;
+	triangle_shader.init(FileManager::read("simple-shader.vs"), FileManager::read("simple-shader.fs"));
 
 	// Setup Dear ImGui context
 	IMGUI_CHECKVERSION();
@@ -100,6 +138,16 @@ int main(int, char **)
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
+		// rendering our geometries
+		triangle_shader.use();
+		glBindVertexArray(vao);
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		triangle_shader.setUniform("color", 1.0f, 1.0f, 1.0f);
+        triangle_shader.setUniform("rotation", 0.0f);
+        triangle_shader.setUniform("translation", 0.0f, 0.0f);		
+		
 		// Render dear imgui into screen
 		ImGui::Render();
 		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
